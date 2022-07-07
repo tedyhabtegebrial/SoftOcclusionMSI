acc8092e395ca592d95d6907954b41cd2d617473
tedyhabtegebrial <tedyhabtegebrial@gmail.com>
2022-07-07 19:48:03 +0200

    added scripts for residential area

--- scripts/residential_scene_00_test.sh
+++ scripts/residential_scene_00_test.sh
@@ -2,18 +2,20 @@ python -m test \
         --ngpus=1 \
         --nnodes=1 \
         --with_pixel_input \
-        --dataset=d3dkit \
+        --dataset=residential \
         --logging_path=exp_logs \
         --dataset_path=somsi_data \
         --num_basis=1 \
-        --num_inputs=9 \
         --batch_size=1 \
         --epochs=300 \
         --decay_step 60 --decay_gamma 0.8 \
         --learning_rate=0.001 \
-        --near=2.5 --far=1000 \
+        --near=0.60 --far=10000 \
+        --num_blocks=7 \
+        --convs_per_block=4 \
         --exp_name=feats_3 \
+        --scene_number=0 \
         --height=320 \
         --width=640 \
         --feats_per_layer=24 \
-        --ckpt_path=ckpts/medieval_port_320x640/medieval_port_feats_24.ckpt
+        --ckpt_path=ckpts/residential_area/scene_00/320x640/feats_24.ckpt \
--- scripts/residential_scene_01.sh
+++ scripts/residential_scene_01.sh
@@ -2,18 +2,20 @@ python -m train \
         --ngpus=1 \
         --nnodes=1 \
         --with_pixel_input \
-        --dataset=d3dkit \
+        --dataset=residential \
         --logging_path=/home/habtegebrial/Desktop/Work/Papers/CVPR22/code_release/exp_logs \
         --dataset_path=somsi_data \
         --exp_name=feats_24 \
         --feats_per_layer=24 \
         --num_basis=1 \
-        --num_inputs=9 \
         --batch_size=1 \
         --epochs=300 \
         --decay_step 60 --decay_gamma 0.8 \
         --learning_rate=0.001 \
-        --near=2.5 --far=1000 \
+        --num_spheres=64 \
+        --near=0.60 --far=20 \
+        --scene_number=1 \
         --height=320 \
         --width=640 \
+        --lambda_mse=1 \
         --convs_per_block=4
