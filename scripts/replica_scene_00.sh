89603d204d32d105f635f8b9620f50c5b4197572
tedyhabtegebrial <tedyhabtegebrial@gmail.com>
2022-07-08 08:53:27 +0200

    added replica script

---
+++ scripts/replica_scene_00.sh
@@ -0,0 +1,21 @@
+python -m train \
+        --ngpus=1 \
+        --nnodes=1 \
+        --with_pixel_input \
+        --dataset=replica \
+        --logging_path=/home/habtegebrial/Desktop/Work/Papers/CVPR22/code_release/exp_logs \
+        --dataset_path=somsi_data \
+        --exp_name=feats_24 \
+        --feats_per_layer=24 \
+        --num_basis=1 \
+        --batch_size=1 \
+        --epochs=300 \
+        --decay_step 60 --decay_gamma 0.8 \
+        --learning_rate=0.001 \
+        --num_spheres=64 \
+        --near=0.60 --far=60 \
+        --scene_number=0 \
+        --height=320 \
+        --width=640 \
+        --lambda_mse=1 \
+        --convs_per_block=4
