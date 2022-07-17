7db60e9de1d58e89cdd4a1b5072bb0e65bed9442
tedyhabtegebrial <tedyhabtegebrial@gmail.com>
2022-07-17 15:29:02 +0200

    alpha composition updated

--- src/alpha_compose.py
+++ src/alpha_compose.py
@@ -23,47 +23,15 @@ class AlphaComposition(nn.Module):
         super(AlphaComposition, self).__init__()
         self.configs = configs
 
-    def forward(self, src_imgs, alpha_imgs, return_weighted_feats=False, return_intermediate=False):
-        # if self.configs.render_mode=='render_3d':
-        #     ones = torch.ones_like(alpha_imgs[:, 0, ...]).unsqueeze(1)
-        #     weight_1 = torch.cumprod(torch.cat([ones, 1-alpha_imgs[:, :-1, :, :, :]], dim=1), dim=1)
-        #     alpha_1 = torch.cat([alpha_imgs[:, :-1, :, :, :], ones], dim=1)
-        #     weighted_imgs = torch.sum(weight_1*alpha_1*src_imgs,dim=1)
-        #     return weighted_imgs
-
-        # else:
+    def forward(self, src_imgs, alpha_imgs):
         alpha_imgs = alpha_imgs.clamp(min=0.0001)
         ones = torch.ones_like(alpha_imgs[:, 0, ...]).unsqueeze(1)
         weight_1 = torch.cumprod(
             torch.cat([ones, 1-alpha_imgs[:, :-1, :, :, :]], dim=1), dim=1)
         weighted_imgs = weight_1*alpha_imgs*src_imgs
-        if return_weighted_feats:
-            num_l = weighted_imgs.shape[1]
-            weighted_feats = [weighted_imgs[:, l, :, :, :] for l in range(num_l)]
-            comb = torch.sum(weighted_imgs, dim=1)
-            return comb, weighted_feats
-        elif return_intermediate:
-            comb = torch.sum(weighted_imgs, dim=1)
-            layer_contribs = weight_1*alpha_imgs
-            return comb, layer_contribs
-        else:
-            comb = torch.sum(weighted_imgs, dim=1)
-            return comb
+        comb = torch.sum(weighted_imgs, dim=1)
+        return comb
 
-    def forward_old(self, src_imgs, alpha_imgs):
-        # alpha_img=[B,D,1,H,W]
-        # src_img=[B,D,C,H,W]
-        # print('Alpha imgs:', src_imgs.data.min(), src_imgs.data.max())
-        # print('Alpha alps:', alpha_imgs.data.min(), alpha_imgs.data.max())
-        b_size, num_d, _c, h, w = src_imgs.shape
-        src_imgs = torch.split(src_imgs, split_size_or_sections=1, dim=1)
-        alpha_imgs = torch.split(alpha_imgs, split_size_or_sections=1, dim=1)
-        comp_rgb = src_imgs[-1]
-        # * alpha_imgs[-1]
-        for d in reversed(range(num_d - 1)):
-            comp_rgb = src_imgs[d] * alpha_imgs[d] + \
-                (1.0 - alpha_imgs[d]) * comp_rgb
-        return comp_rgb.squeeze(1)
 
 if __name__=='__main__':
     device = 'cuda:0'
